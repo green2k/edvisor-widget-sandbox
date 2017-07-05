be2780fb4e4c1b724ffe50f362493d016d581699
Pavol Knapek <knapek.pavol@gmail.com>
2017-07-05 16:53:42 -0700

    CourseFilterScreen refactored to a stateful component

--- src/components/CourseFilterScreen.tsx
+++ src/components/CourseFilterScreen.tsx
@@ -1,37 +1,51 @@
 import * as React from 'react';
 import {Link, withRouter} from 'react-router-dom'
 
-class CourseFilterScreen extends React.Component<any> {
-
-	private iAgeNode;
-	private iLanguageNode;
+class CourseFilterScreen extends React.Component<any, any> {
 
 	constructor(props) {
 		super(props);
+
+		// Initial state
+		this.state = {
+			age: props.age || 22, 
+			language: props.language || ''
+		};
+
+		// Bind private functions
+		this.handleAgeChange = this.handleAgeChange.bind(this);
+		this.handleLanguageChange = this.handleLanguageChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	private handleAgeChange(event) {
+		const newState = this.state;
+		newState.age = event.target.value;
+		this.setState(newState);
+	}
+
+	private handleLanguageChange(event) {
+		const newState = this.state;
+		newState.language = event.target.value;
+		this.setState(newState);
+	}
+
+	// On form-submit, dispatch a Redux action & redirect to "/search"
+	private handleSubmit(event) {
+		event.preventDefault()
+		this.props.onFilterChange(this.state.age, this.state.language)
+		this.props.history.push("/search")
 	}
 
 	render() {
 		return (
 			<div>
-				<form
-					// On form-submit, redirect to "/search"
-					onSubmit={
-						(e) => {
-							e.preventDefault()
-							this.props.history.push("/search")
-						}
-					}
-					// On form-change, dispatch a new "onFilterChange" action
-					onChange={
-						(e) => {
-							this.props.onFilterChange(this.iAgeNode.value, this.iLanguageNode.value)
-						}
-					}>
+				<form onSubmit={this.handleSubmit}>
 					<div>
-						Age: <input name='studentAge' type='text' ref={node => {this.iAgeNode = node}} value={this.props.age} onChange={()=>{}} />
+						Age: <input name='studentAge' type='number' value={this.state.age} onChange={this.handleAgeChange} />
 					</div>
 					<div>
-						Language: <input name='courseLanguage' type='text' ref={node => {this.iLanguageNode = node}} value={this.props.language} onChange={()=>{}} />
+						Language: <input name='courseLanguage' type='text' value={this.state.language} onChange={this.handleLanguageChange} />
 					</div>
 					<div>
 						<input type="submit" value="Search courses!" />
