c3eca00afea6b0af89c8f0590e2484383b5f2bf8
Pavol Knapek <knapek.pavol@gmail.com>
2017-07-07 13:29:33 -0700

    Widget configuration - API key

--- src/index.tsx
+++ src/index.tsx
@@ -7,38 +7,40 @@ import {createStore, combineReducers, applyMiddleware, compose} from 'redux'
 import {courseFiltersReducer} from './reducers/course_filters'
 import App from './components/App'
 
-const networkInterface = createNetworkInterface({
-	uri: 'http://127.0.0.1:5000/graphql'
-})
-
-networkInterface.use([{
-	applyMiddleware(req, next) {
-		if (!req.options.headers) {
-			req.options.headers = {};  // Create the header object if needed.
-		}
-		req.options.headers.authorization = 'agent';
-		next();
-  }
-}]);
-
-// Build Apollo client
-let client = new ApolloClient({networkInterface})
-
-// Create Redux store
-let store = createStore(
-	combineReducers({
-		courseFilter: courseFiltersReducer, 
-		apollo: client.reducer()
-	}), 
-	window['__REDUX_DEVTOOLS_EXTENSION__'] && window['__REDUX_DEVTOOLS_EXTENSION__'](), // Initial state
-	compose(
-		applyMiddleware(client.middleware())
-	)
-);
-
 export const widgets = {
 	courseSearchWidget: {
 		render: (args) => {
+			// Build Apollo-NetworkInterface
+			const networkInterface = createNetworkInterface({
+				uri: 'http://127.0.0.1:5000/graphql'
+			})
+
+			// Apollo-NetworkInterface middleware (API authentication using the APY key)
+			networkInterface.use([{
+				applyMiddleware(req, next) {
+					if (!req.options.headers) {
+						req.options.headers = {};  // Create the header object if needed.
+					}
+					req.options.headers.authorization = args.apiKey;
+					next();
+			  }
+			}]);
+
+			// Build Apollo client
+			let client = new ApolloClient({networkInterface})
+
+			// Create Redux store
+			let store = createStore(
+				combineReducers({
+					courseFilter: courseFiltersReducer, 
+					apollo: client.reducer()
+				}), 
+				window['__REDUX_DEVTOOLS_EXTENSION__'] && window['__REDUX_DEVTOOLS_EXTENSION__'](), // Initial state
+				compose(
+					applyMiddleware(client.middleware())
+				)
+			);
+
 			// Render the application
 			ReactDOM.render(
 				<ApolloProvider store={store} client={client}>
